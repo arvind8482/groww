45993d2236414adb036cbfaa9058e73871aaf5bf
arvind8482 <arvind82.mca@gmail.com>
2024-08-20 11:44:12 +0530

    Image component updated

--- src/app/components/about/what-we-do/page.js
+++ src/app/components/about/what-we-do/page.js
@@ -8,7 +8,7 @@ function WhatWeDo() {
             <div className='container px-6 xl:px-0  mx-auto flex flex-col xl:flex-row xl:items-center'>
             <div className='w-full xl:w-1/2'>
             <Image
-      src="/images/img-whatwedo.png"  className='w-full'  width={''} height={''}
+      src="/images/img-whatwedo.png"  className='w-full'  width={618} height={644}
       alt=" At VisionGroww, we specialize in delivering top-tier IT services to help your business excel in the digital age."
     />
    
@@ -20,9 +20,9 @@ function WhatWeDo() {
                     At VisionGroww, we specialize in delivering top-tier IT services to help your business excel in the digital age. Our diverse offerings include:
                       </p>
                       <ul className='text-default-size list-disc ps-6 py-6'>
-                            <li className='bg-list bg-[left_5px] bg-no-repeat ps-8 pb-6'><strong>Web Design: </strong>We create responsive, user-friendly websites tailored to your business needs, along with stunning visual designs and memorable logos to enhance your online presence. Enjoy robust secure hosting with three months of free maintenance to ensure your website's performance.</li>
+                            <li className='bg-list bg-[left_5px] bg-no-repeat ps-8 pb-6'><strong>Web Design: </strong>We create responsive, user-friendly websites tailored to your business needs, along with stunning visual designs and memorable logos to enhance your online presence. Enjoy robust secure hosting with three months of free maintenance to ensure your website&apos;s performance.</li>
                             <li className='bg-list bg-[left_5px] bg-no-repeat ps-8 pb-6'><strong>App Design (Android/iOS) :</strong> We design intuitive app interfaces and distinctive logos, prioritizing exceptional user experiences. Our app designs also include three months of maintenance for optimal performance.</li>
-                            <li className='bg-list bg-[left_5px] bg-no-repeat ps-8 pb-6'><strong>IT Training :</strong> Our specialized training programs cover Cloud & DevOps (AWS, Azure, GCP), Web Development, AI/ML, and IoT, helping you master essential skills for today's tech landscape.</li>
+                            <li className='bg-list bg-[left_5px] bg-no-repeat ps-8 pb-6'><strong>IT Training :</strong> Our specialized training programs cover Cloud & DevOps (AWS, Azure, GCP), Web Development, AI/ML, and IoT, helping you master essential skills for today&apos;s tech landscape.</li>
                             <li className='bg-list bg-[left_5px] bg-no-repeat ps-8 pb-6'><strong>IT Consultancy :</strong> We provide expert guidance on Cloud Integration, Cloud Migration, DevOps implementation, and web development, ensuring your projects achieve maximum efficiency and success.</li>
                             <li className='bg-list bg-[left_5px] bg-no-repeat ps-8 pb-6'><strong>Master Classes :</strong>  Elevate your expertise with our hands-on master classes in Data Science, essential DevOps tools, Cloud Certification preparation, Full Stack Development, and Database Management.</li>
                         </ul>
@@ -30,7 +30,7 @@ function WhatWeDo() {
               
             </div>
             <p className='text-default-size pt-8 px-6 text-center'>
-                At VisionGroww, we’re dedicated to turning your vision into reality with our commitment to excellence and innovation. Let's build something amazing together!
+                At VisionGroww, we&apos;re dedicated to turning your vision into reality with our commitment to excellence and innovation. Let&apos;s build something amazing together!
                 </p>
         </section>
     </>
