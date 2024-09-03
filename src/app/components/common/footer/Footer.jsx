fec9a445e12fdebbf28ae98470b3c04f89216504
arvind8482 <arvind82.mca@gmail.com>
2024-09-03 09:26:28 +0530

    correction done

--- src/app/components/common/footer/Footer.jsx
+++ src/app/components/common/footer/Footer.jsx
@@ -56,9 +56,9 @@ function Footer() {
             <div className='container mx-auto  text-center border-t border-light mt-4 pt-6'>
             Copyright © visiongroww.com 2024
             </div>
-            <a href='https://wa.me/919457192030' target='_blank' className='fixed right-[10px] bottom-[10px]'>
+            <a href='https://wa.me/919457192030' target='_blank' className='fixed right-[25px] bottom-[25px]'>
                         <Image
-    src='/images/icon-whatsapp.svg' alt='' width={45} height={45}
+    src='/images/icon-whatsapp.svg'   alt='' width={60} height={60}
     /> </a>
  </footer>
     </>
